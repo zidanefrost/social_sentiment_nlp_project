ca4ae505a19c77262d30dce80f2aa7ded163df64
Zidane <uclregzidane@gmail.com>
2025-06-17 14:48:31 +0000

    Add CLI arg for sample size + CSV export

--- sentiment.py
+++ sentiment.py
@@ -1,6 +1,9 @@
 import pandas as pd
 from transformers import pipeline
 import spacy
+import sys
+sample = int(sys.argv[1]) if len(sys.argv) > 1 else 500
+df = pd.read_csv("tweets.csv").dropna(subset=["text"]).head(sample)
 
 df = pd.read_csv("tweets.csv").dropna(subset=["text"]).head(500)
 
@@ -13,3 +16,5 @@ df["sentiment"] = df["text"].apply(lambda t: sent_pipe(t)[0]["label"])
 print(df[["text", "sentiment"]].head())
 nlp = spacy.load("en_core_web_sm")
 df["tokens"] = df["text"].apply(lambda t: [tok.text for tok in nlp(t)])
+df.to_csv("tweets_with_sentiment.csv", index=False)
+print("Saved tweets_with_sentiment.csv")
